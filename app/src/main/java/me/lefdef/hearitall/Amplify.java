9b470b6724160227f31a1aa78825851c48b26f87
lefdef <lefndef@gmail.com>
2014-11-29 13:36:20 -0700

    echo
    echo is distorted, need to adjust playback and sampling

---
+++ app/src/main/java/me/lefdef/hearitall/Amplify.java
@@ -0,0 +1,125 @@
+package me.lefdef.hearitall;
+
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
+import android.media.MediaRecorder;
+import android.media.AudioFormat;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Date;
+
+
+public class Amplify {
+
+    //region CONSTANTS
+    final String TAG = "AMPLIFY";
+    final int SAMPLE_RATE = 44100; // 44100Hz is currently the only rate that is guaranteed to work on all devices, but other rates such as 22050, 16000, and 11025 may work on some devices.
+    // TODO: test CHANNEL_IN_STEREO
+    final int CHANNEL_CONFIG_IN = AudioFormat.CHANNEL_IN_MONO;
+    final int CHANNEL_CONFIG_OUT = AudioFormat.CHANNEL_OUT_STEREO;
+    final int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT; // Guaranteed to be supported by devices. 8BIT is not
+    //endregion
+
+    AudioRecord _audioRecord;
+    AudioTrack _audioTrack;
+    byte[] _buffer;
+
+
+
+    public Amplify() {
+        int inBufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG_IN, AUDIO_FORMAT);
+        int outBufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG_OUT, AUDIO_FORMAT);
+        _audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE, CHANNEL_CONFIG_IN, AUDIO_FORMAT, inBufferSize);
+        // TODO: remove blocking on play
+        _audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE, CHANNEL_CONFIG_OUT, AUDIO_FORMAT, outBufferSize, AudioTrack.MODE_STREAM);
+        _buffer = new byte[inBufferSize];
+    }
+
+    public void startListeningAndPlay() {
+        new AsyncTask<Void, String, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                _audioRecord.startRecording();
+
+                if(_audioRecord.getState() != AudioRecord.STATE_INITIALIZED) {
+                    publishProgress("failed to initialize...");
+                    Log.i(TAG, "failed to initialize...");
+                }
+
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                long lastTime = 0;
+
+                while (AudioRecord.RECORDSTATE_RECORDING == _audioRecord.getRecordingState()) {
+                    int bytesRead = _audioRecord.read(_buffer, 0, _buffer.length);
+                    stream.write(_buffer, 0, bytesRead);
+
+                    if(System.currentTimeMillis() - lastTime > 1000) {
+                        publishProgress(String.format("bytesRead=%1$s, outputStreamSize=%2$s", bytesRead, stream.size()));
+                        Log.i(TAG, String.format("bytesRead=%1$s, outputStreamSize=%2$s", bytesRead, stream.size()));
+                        lastTime = System.currentTimeMillis();
+                    }
+                    if(_audioTrack.getPlayState() != AudioTrack.PLAYSTATE_PLAYING) {
+                        _audioTrack.play();
+                    }
+                    _audioTrack.write(_buffer, 0, bytesRead);
+
+                }
+                publishProgress("");
+
+                return null;
+            }
+
+            @Override
+            protected void onProgressUpdate(String... values) {
+                super.onProgressUpdate(values);
+
+                if(_onRecordStatusChangeListener != null) {
+                    _onRecordStatusChangeListener.onRecordStatusChanged(values[0], _audioRecord.getRecordingState() == AudioRecord.RECORDSTATE_RECORDING);
+                }
+            }
+        }.execute();
+    }
+
+    public void stopListeningAndPlay() {
+        _audioRecord.stop();
+        _audioTrack.pause();
+//        new AsyncTask<Void, String, Void>() {
+//            @Override
+//            protected Void doInBackground(Void... voids) {
+//                _audioRecord.stop();
+//                publishProgress("stopped...");
+//                Log.i(TAG, "stopped...");
+//
+//                return null;
+//            }
+//
+//            @Override
+//            protected void onProgressUpdate(String... values) {
+//                super.onProgressUpdate(values);
+//                if(_onRecordStatusChangeListener != null) {
+//                    _onRecordStatusChangeListener.onRecordStatusChanged(values[0], _audioRecord.getState() == AudioRecord.STATE_INITIALIZED);
+//                }
+//            }
+//        }.execute();
+    }
+
+    //region OnRecordStatusChangeListener
+
+    public interface OnRecordStatusChangeListener {
+        public void onRecordStatusChanged(String message, boolean status);
+    }
+
+    public OnRecordStatusChangeListener _onRecordStatusChangeListener;
+
+    public OnRecordStatusChangeListener getOnRecordStatusChangeListener() {
+        return _onRecordStatusChangeListener;
+    }
+
+    public void setOnRecordStatusChangeListener(OnRecordStatusChangeListener onRecordStatusChangeListener) {
+        this._onRecordStatusChangeListener = onRecordStatusChangeListener;
+    }
+    //endregion
+}
--- app/src/main/java/me/lefdef/hearitall/MainActivity.java
+++ app/src/main/java/me/lefdef/hearitall/MainActivity.java
@@ -4,34 +4,63 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
 
+    final String TAG = "MAINACTIVITY";
+
+    TextView amplifyTextViewState;
     TextView amplifyTextView;
     Switch enable_switch;
+    Amplify _amplify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        _amplify = new Amplify();
+        _amplify.setOnRecordStatusChangeListener(new Amplify.OnRecordStatusChangeListener() {
+            @Override
+            public void onRecordStatusChanged(String message, boolean status) {
+                if(status) {
+                    amplifyTextView.setText(String.format(message));//"Switch enabled? %s", enable_switch.isChecked()));
+                }
+
+                String state = String.format("Status: %s", status ? "Recording" : "Not Recording");
+                amplifyTextViewState.setText(state);
+                Log.i(TAG, state);
+
+            }
+        });
+
         setContentView(R.layout.activity_main);
+
+
+        amplifyTextViewState = (TextView)findViewById(R.id.amplify_textview_state);
         amplifyTextView = (TextView)findViewById(R.id.amplify_textview);
         enable_switch = (Switch)findViewById(R.id.amplify_switch);
 
         enable_switch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                updateTextView();
+                if(enable_switch.isChecked()) {
+                    _amplify.startListeningAndPlay();
+                } else {
+                    _amplify.stopListeningAndPlay();
+                }
             }
         });
     }
 
     private void updateTextView() {
         amplifyTextView.setText(String.format("Switch enabled? %s", enable_switch.isChecked()));
-
     }
 
 //    public void sendAmplifyState(View view) {
