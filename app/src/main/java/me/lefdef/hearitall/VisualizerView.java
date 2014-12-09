d4f46fa22a2492deaf1036a0ea2e602e01c530df
lefdef <lefndef@gmail.com>
2014-12-08 17:35:43 -0700

    forgot to include visualizer view

---
+++ app/src/main/java/me/lefdef/hearitall/VisualizerView.java
@@ -0,0 +1,14 @@
+package me.lefdef.hearitall;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Brigham on 12/1/2014.
+ */
+public class VisualizerView extends View {
+
+    public VisualizerView(Context context) {
+        super(context);
+    }
+}
